ebde811f01e831c1b6279bfd5a04d69f78404205
Gemma Voakes <Gemma.Voakes@ao.com>
2019-04-04 17:28:42 +0100

    Break transcription down into small phrases and add timestamps

--- src/transcribe.js
+++ src/transcribe.js
@@ -27,7 +27,8 @@ async function process() {
 		languageCode: 'en-GB',
 		enableAutomaticPunctuation: true,
 		audioChannelCount: 2,
-		enableSeparateRecognitionPerChannel: true
+		enableSeparateRecognitionPerChannel: true,
+		enableWordTimeOffsets: true
 	};
 
 	const request = {
@@ -40,11 +41,56 @@ async function process() {
 	// Detects speech in the audio file
 	const [operation] = await client.longRunningRecognize(request);
 	const [response] = await operation.promise();
-	const transcription = response.results
-		.map(result => result.alternatives[0].transcript)
-		.join('\n');
-	console.log(`Transcription: ${transcription}`);
 
+	const transcription = response.results.map(result => {
+		// Get a Promise representation of the final result of the job
+
+		response.results.forEach(result => {
+			console.log(`Transcription: ${result.alternatives[0].transcript}`);
+			let phrase = {
+				words: [],
+				startTime: 0,
+				endTime: 0
+			};
+
+			let count = result.alternatives[0].words.length - 1;
+			result.alternatives[0].words.forEach(wordInfo => {
+				// NOTE: If you have a time offset exceeding 2^32 seconds, use the
+				// wordInfo.{x}Time.seconds.high to calculate seconds.
+				const startSecs =
+					`${wordInfo.startTime.seconds}` +
+					`.` +
+					wordInfo.startTime.nanos / 100000000;
+				const endSecs =
+					`${wordInfo.endTime.seconds}` +
+					`.` +
+					wordInfo.endTime.nanos / 100000000;
+
+				if (phrase.words.length < 5) {
+					phrase.words.push(wordInfo.word);
+					if (phrase.words.length === 1) {
+						phrase.startTime = startSecs;
+					}
+					if (count === 0) {
+						phrase.endTime = endSecs;
+						console.log(`${phrase.startTime} --> ${phrase.endTime}`);
+						console.log(phrase.words.join(' '));
+						console.log('');
+					}
+					phrase.endTime = endSecs;
+				} else {
+					console.log(`${phrase.startTime} --> ${phrase.endTime}`);
+					console.log(phrase.words.join(' '));
+					console.log('');
+
+					phrase.words = [wordInfo.word];
+					phrase.startTime = startSecs;
+					phrase.endTime = 0;
+				}
+				count -= 1;
+			});
+		});
+	});
 	return transcription;
 }
 
@@ -53,3 +99,5 @@ exports.transcribe = (req, res) => {
 		res.status(200).send(transcription);
 	});
 };
+
+process();
