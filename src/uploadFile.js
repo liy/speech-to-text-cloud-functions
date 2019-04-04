f29636c2b14d6feedcbee8dd302a174559e6127e
Gregory Scheerlinck <gregory.scheerlinck@gmail.com>
2019-04-04 15:04:04 +0100

    upload file

--- src/uploadFile.js
+++ src/uploadFile.js
@@ -1,4 +1,40 @@
-exports.uploadFile = (req, res) => {
-	console.log(req.body);
-	res.status(200).send('this is the response');
+const { Storage } = require('@google-cloud/storage');
+const storage = new Storage({ projectId: 'speech-to-text-236211' });
+
+const bucket = storage.bucket('speech-to-text-hackday');
+
+
+exports.uploadFile = async (req, res) => {
+	res.set('Access-Control-Allow-Origin', '*');
+
+	if (req.method === 'OPTIONS') {
+    // Send response to OPTIONS requests
+		res.set('Access-Control-Allow-Methods', 'GET');
+		res.set('Access-Control-Allow-Headers', 'Content-Type');
+		res.set('Access-Control-Max-Age', '3600');
+		res.status(204).send('');
+	} else {
+		// Set CORS headers for the main request
+		res.set('Access-Control-Allow-Origin', '*');
+
+    // get the data
+    const { file, token } = Buffer.from(req.body);
+
+    await storage.bucket(bucketName).upload(file, {
+      gzip: true,
+      metadata: {
+        cacheControl: 'public, max-age=31536000',
+      }
+    });
+
+    await storage.bucket(bucketName).upload(token, {
+      gzip: true,
+      metadata: {
+        cacheControl: 'public, max-age=31536000',
+      }
+    });
+
+    console.log('uploaded file')
+    res.status(200).send('this is the response');
+	}
 };
